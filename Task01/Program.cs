3f04315ebefc544114a513119f3cb32870eeed7a
Your Name <you@example.com>
2023-03-29 22:03:08 +0500

    Написана Task03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,11 +3,11 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1==number2*number2)
+if (number1 == number2 * number2)
 {
- Console.WriteLine ($"Ураа число {number1} является квадратом числа {number2}");  
+    Console.WriteLine($"Ураа число {number1} является квадратом числа {number2}");
 }
-else 
+else
 {
- Console.WriteLine ($"па па пам {number1} не является квадратом числа {number2}");
+    Console.WriteLine($"па па пам {number1} не является квадратом числа {number2}");
 }
