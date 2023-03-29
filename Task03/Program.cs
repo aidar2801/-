ef5735059861edaa0e792b58e4a63ea516668c77
Your Name <you@example.com>
2023-03-29 23:08:50 +0500

    написана Task07

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,35 +1,41 @@
-﻿Console.WriteLine("Введите число, которому соответствует день недели: ");
-string day = Console.ReadLine();
-if (day == "1")
+﻿
+internal class Program
 {
-    Console.WriteLine("Понедельник");
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число, которому соответствует день недели: ");
+        string day = Console.ReadLine();
+        if (day == "1")
+        {
+            Console.WriteLine("Понедельник");
+        }
+        else if (day == "2")
+        {
+            Console.WriteLine("Вторник");
+        }
+        else if (day == "3")
+        {
+            Console.WriteLine("Среда");
+        }
+        else if (day == "4")
+        {
+            Console.WriteLine("Четверг");
+        }
+        else if (day == "5")
+        {
+            Console.WriteLine("Пятница");
+        }
+        else if (day == "6")
+        {
+            Console.WriteLine("Суббота");
+        }
+        else if (day == "7")
+        {
+            Console.WriteLine("Воскресенье");
+        }
+        else
+        {
+            Console.WriteLine("это не день недели");
+        }
+    }
 }
-else if (day == "2")
-{
-    Console.WriteLine("Вторник");
-}
-else if (day == "3")
-{
-    Console.WriteLine("Среда");
-}
-else if (day == "4")
-{
-    Console.WriteLine("Четверг");
-}
-else if (day == "5")
-{
-    Console.WriteLine("Пятница");
-}
-else if (day == "6")
-{
-    Console.WriteLine("Суббота");
-}
-else if (day == "7")
-{
-    Console.WriteLine("Воскресенье");
-}
-else
-{
-    Console.WriteLine("это не день недели");
-}
-
---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>=100 && number<=999)
+{
+int lastNumber = number %10;
+Console.WriteLine($"Последняя цифра числа: {lastNumber}");
+}
+else {
+Console.WriteLine($" {number} число не трехзначное ");
+}
