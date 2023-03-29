e1af4b06bdfbf686d8e416b709f34c0e7a0ccd0f
Your Name <you@example.com>
2023-03-29 20:32:35 +0500

    Написана программа 01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1==number2*number2)
+{
+ Console.WriteLine ($"Ураа число {number1} является квадратом числа {number2}");  
+}
+else 
+{
+ Console.WriteLine ($"па па пам {number1} не является квадратом числа {number2}");
+}
