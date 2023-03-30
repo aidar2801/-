20433683ea65afbade04117413945add1e8dec24
Your Name <you@example.com>
2023-03-30 20:36:21 +0500

    написана Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB && numberA>numberC)
+{
+    Console.WriteLine($"{numberA} Максимальное из трех чисел");
+}
+if (numberB>numberA && numberB>numberC)
+{
+    Console.WriteLine($"{numberB} Максимальное из трех чисел");
+}
+if (numberC>numberA && numberC>numberB)
+{
+    Console.WriteLine($"{numberC} Максимальное из трех чисел");
+}
+
