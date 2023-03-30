9579f455d09d2909d2788d2359c201c5d8a9efac
Your Name <you@example.com>
2023-03-30 21:40:12 +0500

    написана Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=number;i++)
+{
+if (i % 2 == 0)
+System.Console.Write(i+ ", ");
+}
